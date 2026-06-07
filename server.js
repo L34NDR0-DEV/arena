688a17618bbc9d0e07ba58ca7b6258104dcc744b
Leandro <leandrosilva212010@gmail.com>
2026-06-07 01:56:18 -0300

    Tornar porta configuravel via process.env.PORT (necessario para hosts como Render/Railway)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 const fs     = require('fs');
 const path   = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const ROOT = __dirname;
 
 const MIME = {
